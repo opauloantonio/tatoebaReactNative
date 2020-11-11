241d2874c68efe9461132c71d30bb16088196728
Paulo Antonio <anj.heo@gmail.com>
2020-11-11 20:02:59 -0300

    Added error message and a mechanism to stop further requests if we reach the end of the search results.

--- app/screens/SearchResults/SearchResults.js
+++ app/screens/SearchResults/SearchResults.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 import { View, Text, FlatList } from 'react-native';
 
-import { ActivityIndicator } from 'react-native-paper';
+import { ActivityIndicator, Button } from 'react-native-paper';
 
 import SentenceContainer from '../../components/SentenceContainer';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import axios from 'axios';
 
@@ -13,16 +15,24 @@ import api from '../../api';
 const SearchResults = props => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [loadingMore, setLoadingMore] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [endOfSearch, setEndOfSearch] = useState(false);
+  const [page, setPage] = useState(1);
 
   const [data, setData] = useState({
     numberOfResults: "",
     sentences: [],
   });
 
-  const [page, setPage] = useState(1);
-
   const fetchSentences = () => {
+    if (loadingMore) {
+      return;
+    }
+
+    if (page > 1 && endOfSearch) {
+      return;
+    }
+
     const {from, to, text} = props.route.params;
 
     if (page === 1) {
@@ -46,7 +56,11 @@ const SearchResults = props => {
       setError(false);
       setLoading(false);
       setLoadingMore(false);
-      setPage(page + 1)
+      setPage(page + 1);
+
+      if (res.data.sentences.length === 0) {
+        setEndOfSearch(true);
+      }
     }).catch((e) => {
       setError(true);
       setLoading(false);
@@ -69,7 +83,17 @@ const SearchResults = props => {
   } else if (error) {
     return(
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>error</Text>
+        <Icon name="warning" color="#8B0000" size={100} />
+        
+        <Text style={{marginVertical: 20}}>
+          There was an error while trying to fetch your search results
+        </Text>
+        
+        <Button mode="contained" onPress={fetchSentences}>
+          <Text style={{color: "white"}}>
+            RETRY
+          </Text>
+        </Button>
       </View>
     );
   } else {
@@ -83,7 +107,7 @@ const SearchResults = props => {
           renderItem={({ item }) => <SentenceContainer sentence={item} />}
           keyExtractor={(item) => item.id.toString()}
           data={data.sentences}
-          onEndReached={() => fetchSentences()}
+          onEndReached={fetchSentences}
           onEndReachedThreshold={0.1}
         />
 
