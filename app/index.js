39a527eb435c74469e10efc3f03553e686310ecf
Paulo Antonio <anj.heo@gmail.com>
2020-11-13 18:21:10 -0300

    Integrated store and redux-persist

--- app/index.js
+++ app/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 import Tabs from './navigation';
 
+import { View } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, ActivityIndicator, Provider as PaperProvider } from 'react-native-paper';
+
+import { Provider as ReduxProvider } from 'react-redux';
+
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store';
 
 const theme = {
   ...DefaultTheme,
@@ -14,12 +22,22 @@ const theme = {
   },
 };
 
+const LoadingScreen = () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <ActivityIndicator animating={true} color="#4caf50"/>
+  </View>
+)
+
 const App = () => (
-  <PaperProvider theme={theme}>
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
-  </PaperProvider>
+  <ReduxProvider store={store}>
+    <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Tabs />
+        </NavigationContainer>
+      </PaperProvider>
+    </PersistGate>
+  </ReduxProvider>
 );
 
 export default App;
