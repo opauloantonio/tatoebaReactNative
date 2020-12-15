e6ecead4b7534d61daf7dbde08932b5d12ede890
Paulo Antonio <anj.heo@gmail.com>
2020-12-15 20:22:25 -0300

    Drafting History screen

--- app/screens/History/History.js
+++ app/screens/History/History.js
@@ -2,10 +2,34 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
-const History = () => (
-  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <Text>History Screen</Text>
-  </View>
-);
+import { connect } from 'react-redux';
 
-export default History;
+import { addEntry, removeEntry, clearHistory } from '../../actions/historyActions';
+
+const History = props => {
+  if (props.searchHistory.history.length === 0) {
+    return(
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Your search history will appear here.</Text>
+      </View>
+    );
+  } else {
+    return(
+      <View style={{flex: 1}}>
+        
+      </View>
+    )
+  }
+};
+
+const mapStateToProps = state => ({
+  searchHistory: state.historyReducer,
+});
+
+const mapDispatchToProps = dispatch => ({
+  addEntry: entry => dispatch(addEntry(entry)),
+  removeEntry: id => dispatch(removeEntry(id)),
+  clearHistory: () => dispatch(clearHistory()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(History);
