ae20e7044876ee03dbb1ec99eeffd54d3e891909
Paulo Antonio <anj.heo@gmail.com>
2021-09-27 13:44:38 -0300

    Moved PersistGate up

--- App.js
+++ App.js
@@ -2,14 +2,28 @@ import React from 'react';
 
 import App from './app/index';
 
+import { View } from 'react-native';
+
+import { ActivityIndicator } from 'react-native-paper';
+
 import { Provider as ReduxProvider } from 'react-redux';
 
-import { store } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './app/store';
+
+const LoadingScreen = () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <ActivityIndicator animating={true} color="#4caf50"/>
+  </View>
+);
 
 const StoreWrapper = () => (
   <ReduxProvider store={store}>
-    <App />
+    <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+      <App />
+    </PersistGate>
   </ReduxProvider>
-)
+);
 
 export default StoreWrapper;
--- app/index.js
+++ app/index.js
@@ -2,15 +2,9 @@ import React, { useEffect } from 'react';
 
 import Tabs from './navigation';
 
-import { View } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
 
-import { ActivityIndicator, Provider as PaperProvider } from 'react-native-paper';
-
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { persistor } from './store';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import SplashScreen from 'react-native-splash-screen';
 
@@ -20,12 +14,6 @@ import useTheme from './utils/useTheme';
 
 const themes = { dark: darkTheme, light: lightTheme };
 
-const LoadingScreen = () => (
-  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <ActivityIndicator animating={true} color="#4caf50"/>
-  </View>
-);
-
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
@@ -34,13 +22,11 @@ const App = () => {
   const theme = themes[useTheme()];
 
   return(
-    <PersistGate loading={<LoadingScreen />} persistor={persistor}>
-      <PaperProvider theme={theme}>
-        <NavigationContainer theme={theme}>
-          <Tabs theme={theme} />
-        </NavigationContainer>
-      </PaperProvider>
-    </PersistGate>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <Tabs theme={theme} />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
